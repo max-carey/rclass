d38e76679833d8be64d2fc142b77ab806945fd3a
Max Carey <maxcarey@protonmail.com>
2019-03-01 14:32:12 -0600

    Modify slides for Friday March 1st class

--- docs/unidadDos/starterPack/myScript.R
+++ docs/unidadDos/starterPack/myScript.R
@@ -1,1 +1,31 @@
-# This is a comment
+# Cargar paquetes
+library(dplyr)
+library(ggplot2)
+
+# Cargar datos
+spanLexData <- read.csv("spanLexData.csv")
+
+# Analizar datos
+str(spanLexData)
+glimpse(spanLexData)
+
+# Ver los niveles de la columna pos
+unique(spanLexData$pos)
+
+# Mostrar los primeros 100 observaciones
+head(spanLexData, n=100)
+
+# Reordernar según rank
+spanLexData <- spanLexData %>%
+  arrange(rank)
+
+# Crear gráfica
+ggplot(spanLexData, aes(y=rawFreq,x=rank,color=pos)) +
+  geom_point() +
+  scale_y_log10()
+
+# Ver por una gráfica de dispersión por pos
+ggplot(spanLexData, aes(y=rawFreq,x=rank)) +
+  geom_point() +
+  scale_y_log10() +
+  facet_wrap(~pos)
